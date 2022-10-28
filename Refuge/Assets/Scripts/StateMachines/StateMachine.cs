4dca3bda22ffff3b4c36a33ac3fa015c85985420
Jens Varga <jens.varga@2xper.se>
2022-10-28 10:15:31 +0200

    Update networking authority for character prefab.

--- Refuge/Assets/Scripts/StateMachines/StateMachine.cs
+++ Refuge/Assets/Scripts/StateMachines/StateMachine.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public abstract class StateMachine : MonoBehaviour
+public abstract class StateMachine : NetworkBehaviour
 {
   private State currentState;
 
     private void Update()
     {
+      if (!hasAuthority) { return; }
+
       currentState?.Tick(Time.deltaTime);
     }
 
     public void SwitchState(State newState)
     {
+      if (!hasAuthority) { return; }
+      
       currentState?.Exit();
       currentState = newState;
       currentState?.Enter();
